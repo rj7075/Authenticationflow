e1512e2b434acfb873ac6704844a6bc79df63071
rj7075 <128252566+rj7075@users.noreply.github.com>
2025-02-01 23:50:12 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -26,7 +26,7 @@ function Login() {
       return handleError("email and password are required");
     }
     try {
-      const url = `https://authenticationflowapi.vercel.app/login`;
+      const url = `https://authenticationflowapi.vercel.app/auth/login`;
       const response = await fetch(url, {
         method: "POST",
         headers: {
